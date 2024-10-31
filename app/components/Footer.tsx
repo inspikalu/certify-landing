2e3848d9dd92d2a66b3829491e253e8d931dd392
Okereke Clement Kalu <inspikalu@gmail.com>
2024-11-01 00:33:05 +0100

    Added some position styles

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="w-full bg-[#BB86FC] rounded-t-[50px] min-h-[10rem] p-[3rem] flex flex-col items-center justify-center">
       <span className="font-bold text-center text-[#333333] text-xl">Simply download the app and get to enjoy confidence in your products</span>
       <div>
-        <div className="flex flex-wrap gap-4 mt-4">
+        <div className="flex flex-wrap items-center justify-center gap-4 mt-4">
           <a href="#" className="block w-[140px] h-[42px] relative">
             <Image 
               src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Download_on_the_App_Store_Badge.svg"
